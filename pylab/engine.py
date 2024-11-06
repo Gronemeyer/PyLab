a577cc3b4bb9e34ac12edee898603b8369017eff
Gronemeyer <46824871+Gronemeyer@users.noreply.github.com>
2024-11-06 16:59:47 -0500

    Updated v0.7.0 Engine Fix to Pymmcore-plus v0.12.0
    - Added detailed logging to independent engines
    - Added new logic to avoid early sequence break and memory buffer overflows

--- pylab/engine.py
+++ pylab/engine.py
@@ -1,9 +1,8 @@
+import pymmcore_plus
 import useq
 import time
 
-
 from typing import TYPE_CHECKING, Iterable, Mapping
-
 
 if TYPE_CHECKING:
     from pymmcore_plus.core._sequencing import SequencedEvent
@@ -13,31 +12,29 @@ if TYPE_CHECKING:
     PImagePayload = tuple[NDArray, MDAEvent, FrameMetaV1]
 from pymmcore_plus.mda import MDAEngine
 
+from itertools import product
+
+import logging
+logging.basicConfig(filename='engine.log', level=logging.DEBUG, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 class PupilEngine(MDAEngine):
     
-    def setup_event(self, event: useq.MDAEvent) -> None:
-        """Prepare state of system (hardware, etc.) for `event`."""
-        # do some custom pre-setup
-        print('START: pupil custom setup_event')
-        super().setup_event(event)  
-        print('DONE: pupil custom setup_event')
-
-        # do some custom post-setup
-
-    def exec_event(self, event: useq.MDAEvent) -> object:
-        """Prepare state of system (hardware, etc.) for `event`."""
-        # do some custom pre-execution
-        print(f'START: pupil custom exec_event \n Event type: {type(event)}')
-        result = super().exec_event(event)  
-        print(f'DONE: pupil custom exec_event \n Event type: {type(event)}')
-
-        # do some custom post-execution
-        return result
-    
     def exec_sequenced_event(self, event: 'SequencedEvent') -> Iterable['PImagePayload']:
+        """Execute a sequenced (triggered) event and return the image data.
+
+        This method is not part of the PMDAEngine protocol (it is called by
+        `exec_event`, which *is* part of the protocol), but it is made public
+        in case a user wants to subclass this engine and override this method.
+        
+        custom override sequencerunning loop jgronemeyer24
+        """
         n_events = len(event.events)
 
+        t0 = event.metadata.get("runner_t0") or time.perf_counter()
+        event_t0_ms = (time.perf_counter() - t0) * 1000
+        
         # Start sequence
         # Note that the overload of startSequenceAcquisition that takes a camera
         # label does NOT automatically initialize a circular buffer.  So if this call
@@ -47,67 +44,66 @@ class PupilEngine(MDAEngine):
             0,  # intervalMS  # TODO: add support for this
             True,  # stopOnOverflow
         )
-
+        logging.info(f'{self.__str__()} exec_sequenced_event with {n_events} events at t0 {t0}')
         self.post_sequence_started(event)
 
+        n_channels = self._mmc.getNumberOfCameraChannels()
         count = 0
-        iter_events = iter(event.events)
+        iter_events = product(event.events, range(n_channels))
         # block until the sequence is done, popping images in the meantime
-        while self._mmc.isSequenceRunning():
-            if self._mmc.getRemainingImageCount():
-                yield self._next_img_payload(next(iter_events))
-                count += 1
+        while True:
+            if self._mmc.isSequenceRunning():
+                if remaining := self._mmc.getRemainingImageCount():
+                    yield self._next_seqimg_payload(
+                        *next(iter_events), remaining=remaining - 1, event_t0=event_t0_ms
+                    )
+                    count += 1
+                else:
+                    if count == n_events:
+                        logging.debug(f'{self.__str__()} stopped MDA: \n{self._mmc} with \n{count} events and \n{remaining} remaining with \n{self._mmc.getRemainingImageCount()} images in buffer')
+                        break
+                        #self._mmc.stopSequenceAcquisition() 
+                    time.sleep(0.001)
             else:
-                time.sleep(0.001)
-                if count == n_events:
-                    self._mmc.stopSequenceAcquisition()
-                    print(f'stopped pupil MDA image overflow: {self._mmc}')
+                break
 
         if self._mmc.isBufferOverflowed():  # pragma: no cover
+            logging.debug(f'OVERFLOW {self.__str__()} MDA: \n{self._mmc} with \n{count} events and \n{remaining} remaining with \n{self._mmc.getRemainingImageCount()} images in buffer')
             raise MemoryError("Buffer overflowed")
 
-        while self._mmc.getRemainingImageCount():
-            yield self._next_img_payload(next(iter_events))
+        while remaining := self._mmc.getRemainingImageCount():
+            logging.debug(f'{self.__str__()} Saving Remaining Images in buffer \n{self._mmc} with \n{count} events and \n{remaining} remaining with \n{self._mmc.getRemainingImageCount()} images in buffer')
+            yield self._next_seqimg_payload(
+                *next(iter_events), remaining=remaining - 1, event_t0=event_t0_ms
+            )
             count += 1
-
     
     def teardown_sequence(self, sequence: useq.MDASequence) -> None:
         """Perform any teardown required after the sequence has been executed."""
-        #core = super().mmcore
-        #core.stopSequenceAcquisition
-        print('DONE: pupil custom teardown')
-        #mmcore_dhyana.stopSequenceAcquisition
+        logging.info(f'PupilEngine teardown_sequence at time: {time.time()}')
         pass
     
 class MesoEngine(MDAEngine):
-    
-    def setup_sequence(self, sequence: useq.MDASequence) -> Mapping[str, any]:
-        """Setup the hardware for the entire sequence."""
-        # clear z_correction for new sequence
-        print('START: meso setup_sequence')
-        super().setup_sequence(sequence)
-    
-    def setup_event(self, event: useq.MDAEvent) -> None:
-        """Prepare state of system (hardware, etc.) for `event`."""
-        # do some custom pre-setup
-        print('START: meso custom setup_event')
-        super().setup_event(event)  
-        print('DONE: meso custom setup_event')
-
-        # do some custom post-setup
-
-    def exec_event(self, event: useq.MDAEvent) -> object:
-        """Prepare state of system (hardware, etc.) for `event`."""
-        # do some custom pre-execution
-        print('START: meso custom exec_event')
-        result = super().exec_event(event)  
-        print('DONE: meso custom exec_event')
-
-        # do some custom post-execution
-        return result
+    # def __init__(self, mmc: pymmcore_plus.CMMCorePlus, use_hardware_sequencing: bool = True) -> None:
+    #     super().__init__(mmc)
+    #     self._mmc = mmc
+    #     self.use_hardware_sequencing = use_hardware_sequencing
+    #     self._meso_counter = None
+    #     self._meso_metadata = {}
     
     def exec_sequenced_event(self, event: 'SequencedEvent') -> Iterable['PImagePayload']:
+        """Execute a sequenced (triggered) event and return the image data.
+
+        This method is not part of the PMDAEngine protocol (it is called by
+        `exec_event`, which *is* part of the protocol), but it is made public
+        in case a user wants to subclass this engine and override this method.
+        
+        custom override sequencerunning loop jgronemeyer24
+        """
         n_events = len(event.events)
+
+        t0 = event.metadata.get("runner_t0") or time.perf_counter()
+        event_t0_ms = (time.perf_counter() - t0) * 1000
 
         # Start sequence
         # Note that the overload of startSequenceAcquisition that takes a camera
@@ -118,34 +114,39 @@ class MesoEngine(MDAEngine):
             0,  # intervalMS  # TODO: add support for this
             True,  # stopOnOverflow
         )
-
+        logging.info(f'{self.__str__()} exec_sequenced_event with {n_events} events at t0 {t0}')
         self.post_sequence_started(event)
 
+        n_channels = self._mmc.getNumberOfCameraChannels()
         count = 0
-        iter_events = iter(event.events)
+        iter_events = product(event.events, range(n_channels))
         # block until the sequence is done, popping images in the meantime
         while self._mmc.isSequenceRunning():
-            if self._mmc.getRemainingImageCount():
-                yield self._next_img_payload(next(iter_events))
+            if remaining := self._mmc.getRemainingImageCount():
+                yield self._next_seqimg_payload(
+                    *next(iter_events), remaining=remaining - 1, event_t0=event_t0_ms
+                )
                 count += 1
             else:
-                time.sleep(0.001)
                 if count == n_events:
-                    self._mmc.stopSequenceAcquisition()
-                    print(f'stopped pupil MDA image overflow: {self._mmc}')
+                    logging.debug(f'{self.__str__()} stopped MDA: \n{self._mmc} with \n{count} events and \n{remaining} remaining with \n{self._mmc.getRemainingImageCount()} images in buffer')                 
+                    break
+                    #self._mmc.stopSequenceAcquisition() Might be source of early cutoff by not allowing engine to save the rest of image in buffer
+                #time.sleep(0.001) #does not seem to optimize performance either way
 
         if self._mmc.isBufferOverflowed():  # pragma: no cover
+            logging.debug(f'OVERFLOW {self.__str__()}; Images in buffer: {self.mmcore.getRemainingImageCount()}')
             raise MemoryError("Buffer overflowed")
 
-        while self._mmc.getRemainingImageCount():
-            yield self._next_img_payload(next(iter_events))
+        while remaining := self._mmc.getRemainingImageCount():
+            logging.debug(f'{self.__str__()} Saving Remaining Images in buffer \n{self._mmc} with \n{count} events and \n{remaining} remaining with \n{self._mmc.getRemainingImageCount()} images in buffer')
+            yield self._next_seqimg_payload(
+                *next(iter_events), remaining=remaining - 1, event_t0=event_t0_ms
+            )
             count += 1
     
     def teardown_sequence(self, sequence: useq.MDASequence) -> None:
         """Perform any teardown required after the sequence has been executed."""
-        #core = super().mmcore
-        #core.stopSequenceAcquisition
-        print('DONE: meso custom teardown')
-        #mmcore_thor.stopSequenceAcquisition
+        logging.info(f'MesoEngine teardown_sequence at time: {time.time()}')
         pass
 
